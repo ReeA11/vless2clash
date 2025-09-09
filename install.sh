cddc5c765c8f0a51bcfb3ce1b7bd7c90f4cf14ed
Ree All <rimskiy1998@gmail.com>
2025-09-09 16:41:56 +0300

    Project init

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 TMP_FILE=$(mktemp)
 
 # Скачиваем Python-скрипт с GitHub
-curl -sL "https://raw.githubusercontent.com/<YOUR_GITHUB_USERNAME>/vless2clash/main/vless2clash.py" -o "$TMP_FILE"
+curl -sL "https://raw.githubusercontent.com/ReeA11/vless2clash/refs/heads/master/vless2clash.py" -o "$TMP_FILE"
 
 # Запрашиваем у пользователя ссылку VLESS
 read -p "Введите VLESS ссылку: " VLESS_URL
